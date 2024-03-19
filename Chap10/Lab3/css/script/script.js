daf1075d64e26b7ac998040a8fb2295a0a44df8c
Wllhtet <156835682+Wllhtet@users.noreply.github.com>
2024-03-19 03:09:23 -0400

    Add files via upload

---
+++ Chap10/Lab3/css/script/script.js
@@ -0,0 +1,8 @@
+function Hamburger(){
+    var menu = document.getElementById("nav__link"); 
+    if(menu.style.display === "block"){
+        menu.style.display = "none";
+    }   else{
+        menu.style.display = "block"; 
+    }
+} 
